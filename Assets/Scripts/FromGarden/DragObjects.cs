6563ee5e3bff4d852721ef1ca2b0a872ce676e58
FlyingJudgement <flyingjudgement@gmail.com>
2021-02-04 15:09:13 +0000

    TechDebt Refactoring GridSnap to be more clear. Importing From SoundGarden under eachfolder as "Garden" Scripts, Audio, Prefabs, Textures, and Some of the Materials

---
+++ Assets/Scripts/FromGarden/DragObjects.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+
+public class DragObjects : MonoBehaviour
+{
+
+    float ZPosition;
+    Vector3 Offset;
+    bool Dragging;
+
+    public Camera mainCamera;
+    [SerializeField]
+    public UnityEvent OnBeginDrag;
+    [SerializeField]
+    public UnityEvent OnEndDrag;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+        ZPosition = mainCamera.WorldToScreenPoint(transform.position).z;
+        //z axis of the game object
+    }
+
+    void Update()
+    {
+        if(Dragging) 
+        {
+            Vector3 position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, ZPosition);
+            transform.position = mainCamera.ScreenToWorldPoint(position + new Vector3(Offset.x, Offset.y));
+
+        }
+    }
+    void OnMouseDown()
+    {
+        if(!Dragging)
+        {
+            BeginDrag();
+        }
+    }
+
+    void OnMouseUp()
+    {
+        EndDrag();
+    }
+
+    public void BeginDrag()
+    {
+        OnBeginDrag.Invoke();
+        Dragging = true;
+        Offset = mainCamera.WorldToScreenPoint(transform.position) - Input.mousePosition;
+    }
+
+    public void EndDrag()
+    {
+        OnEndDrag.Invoke();
+        Dragging = false;
+    }
+  
+}
--- Assets/Scripts/GridSnaper.cs
+++ Assets/Scripts/GridSnaper.cs
@@ -12,16 +12,25 @@ public class GridSnaper : MonoBehaviour
     Vector3 snapPos;
     void Update()
     {
+        SnapPlanesByIncrement();
+        WritePositionOnObjectAndName();
+    }
+
+    private void SnapPlanesByIncrement()
+    {
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 
         transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
-        
+    }
+
+    private void WritePositionOnObjectAndName()
+    {
         textMesh = GetComponentInChildren<TextMesh>();
-        string cordinates = 
-            (snapPos.x /gridSize + "." 
-            + snapPos.z / gridSize + 
+        string cordinates =
+            (snapPos.x / gridSize + "."
+            + snapPos.z / gridSize +
             System.Environment.NewLine + snapPos.y / gridSize);
         textMesh.text = cordinates;
         gameObject.name = cordinates;
